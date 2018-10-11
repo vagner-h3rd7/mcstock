c185b0ba0a0c53768802416abf495bf9c5538637
Vagner H <vagner_h3rd7@outlook.com>
2018-10-11 16:36:05 -0300

    criando formulario

--- src/app/component/compt-form/compt-form.component.ts
+++ src/app/component/compt-form/compt-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Compt } from '../../models/compt';
 
 @Component({
   selector: 'app-compt-form',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComptFormComponent implements OnInit {
 
+  brand: string; 
+  model: string; 
+  motherboard: string; 
+  processor: string;
+  memory_ram: string; 
+  hard_disck: string;
+  uri_photo: string; 
+
+  @Output() comptAdded = new EventEmitter<Compt>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addCompt() {
+    this.comptAdded.emit({
+      brand: this.brand, 
+      model: this.model, 
+      motherboard: this.motherboard, 
+      processor: this.processor,
+      memory_ram: this.memory_ram, 
+      hard_disck: this.hard_disck,
+      uri_photo: this.uri_photo, 
+	    hide: true
+    })
+
   }
 
 }

b698d2e28e9dcb311e08f5946aa3460de80f5460
Vagner H <vagner_h3rd7@outlook.com>
2018-10-11 14:19:44 -0300

    adicionando interface e modelo de dados

---
+++ src/app/models/compt.ts
@@ -0,0 +1,10 @@
+export interface Compt {
+    brand: string, 
+    model: string, 
+    motherboard: string, 
+    processor: string,
+    memory_ram: string, 
+    hard_disck: string,
+    uri_photo: string, 
+    hide: boolean;
+}
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { Compt } from '../models/compt';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  compts: Compt[];
 
   constructor() { }
 }

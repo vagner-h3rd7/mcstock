a6efb54b2cfb53cbcc5831bf081482b8562292d5
Vagner H <vagner_h3rd7@outlook.com>
2018-10-11 23:21:34 -0300

    teste

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
 import { Compt } from '../models/compt';
+
 @Injectable({ providedIn: 'root' })
 export class DataService {
-  compts: Compt[];
+  
+  // exemplo
+  compts: Compt[] = [
+    { brand: 'sPc1', model: 'exemp sPcPro1', motherboard: 'proprietary-1', processor: 'itl y8 7800 - 3.00 GHz', memory_ram: 'KingsRam - 8GB', hard_disck: 'SanHD - 1 TB', uri_photo: 'src\\assets\\img\\img_01.jpg', hide: true},
+    { brand: 'sPc2', model: 'exemp sPcPro2', motherboard: 'proprietary-2', processor: 'itl y8 7800 - 3.00 GHz', memory_ram: 'KingsRam - 4GB', hard_disck: 'SanHD - 2 TB', uri_photo: 'src\\assets\\img\\img_02.jpg', hide: true},
+    { brand: 'sPc3', model: 'exemp sPcPro3', motherboard: 'proprietary-3', processor: 'itl y9 7900 - 6.00 GHz', memory_ram: 'KingsRam - 16GB', hard_disck: 'SanHD - 1 TB', uri_photo: 'src\\assets\\img\\img_03.jpg', hide: true},
+    { brand: 'sPc4', model: 'exemp sPcPro4', motherboard: 'proprietary-4', processor: 'itl y8 7800 - 3.00 GHz', memory_ram: 'KingsRam - 4GB', hard_disck: 'SanHD - 4 TB', uri_photo: 'src\\assets\\img\\img_04.jpg', hide: true}
+  ];
+
 
   constructor() { 
     this.compts = [];

90cc69c591cb07cfb846098f8a3d00919d7719f2
Vagner H <vagner_h3rd7@outlook.com>
2018-10-14 18:04:22 -0300

    sistema de busca implementado

--- src/app/component/compt-search/compt-search.component.ts
+++ src/app/component/compt-search/compt-search.component.ts
@@ -10,11 +10,25 @@ import { DataService } from '../../services/data.service';
 })
 export class ComptSearchComponent implements OnInit {
   @Input() compt: Compt;
-
+  compts: Compt[];
+  filteredCompts: Compt[];
+  searchString: string;
 
   constructor(public dataService: DataService) { }
 
   ngOnInit() {
+  }
+  
+  onSearch(e, value): void {
+    this.compts = this.dataService.getCompt();
+
+    this.searchString = e.target.value;
+    const searchType = 'brand';
+
+    this.filteredCompts = this.compts.filter(filteredCompt =>
+      new RegExp(this.searchString.toLocaleLowerCase())
+        .test(filteredCompt[searchType].toLocaleLowerCase())
+    );
   }
 
 }

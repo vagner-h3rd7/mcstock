c91279a175301fe30c9533889858723660e7a7fd
Vagner H <vagner_h3rd7@outlook.com>
2018-10-26 21:55:44 -0300

    pagina de erro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ComptComponent } from './component/compt/compt.component';
 import { ComptListComponent } from './component/compt-list/compt-list.component';
 import { ComptFormComponent } from './component/compt-form/compt-form.component';
@@ -11,11 +12,13 @@ import { ComptTableComponent } from './component/compt-table/compt-table.compone
 const routes: Routes = [
   { path: '', redirectTo: '/painel', pathMatch: 'full' },
   { path: 'painel', component: ComptListComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: 'search', component: ComptSearchComponent },
   { path: 'table', component: ComptTableComponent },
   { path: 'new', component: ComptFormComponent },
   { path: 'detail/:id', component: ComptComponent },
-  { path: 'edit/:id', component: ComptFormComponent }
+  { path: 'edit/:id', component: ComptFormComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
